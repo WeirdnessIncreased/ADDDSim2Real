0f43b783296716aa8b42c20ec9f354a3cc0f0c26
LarryZhang <u3594786@connect.hku.hk>
2022-07-07 10:28:22 +0800

    Update map builder

--- RRT/rrt_test.py
+++ RRT/rrt_test.py
@@ -180,12 +180,81 @@ def rapidlyExploringRandomTree(ax, img, start, goal, seed=None):
     ppl.show()
     return path
 
+def get_obstacle():
+    fixed_obstacle = {
+        'B1': [ 7.08, 1.00, 8.08, 1.2],
+        'B2': [ 5.78, 2.14 ,6.58 , 2.34], 
+        'B3': [ 6.58, 3.48, 6.78, 4.48],
+        'B4': [ 3.54, 0.935, 4.45 , 1.135], 
+        'B5': [ 3.864, 2.064, 4.216, 2.416], 
+        'B6': [ 3.54, 3.345, 4.45 , 3.545],
+        'B7': [ 1.5, 0, 1.7, 1],
+        'B8': [ 1.5, 2.14, 2.3, 2.34], 
+        'B9': [ 0, 3.28, 1, 3.48],
+        'B10': [ 0, 0, 0.02, 4.48 ],
+        'B11': [ 8.08, 0, 8.1, 4.48],
+        'B12': [ 0, 0, 8.1, 0.02 ],
+        'B13': [ 0, 4.48, 8.08, 4.50 ]
+    }
+
+    ox1, oy1, ox2, oy2 = [], [], [], []
+
+    for name in fixed_obstacle:
+        ox1.append( fixed_obstacle[name][0] / 0.02 )
+        oy1.append( fixed_obstacle[name][1] / 0.02 )
+        ox2.append( fixed_obstacle[name][2] / 0.02 )
+        oy2.append( fixed_obstacle[name][3] / 0.02 )
+
+    obstacles = list(zip(ox1, oy1, ox2, oy2))
+    # 8.08 / 0.02 = 404
+    # 4.48 / 0.02 = 224
+    obstacle_map = np.full( ( 405, 225 ) , 255 )
+    
+    for pos in obstacles:
+        for x in np.arange( pos[0], pos[2] ):
+            for y in np.arange( pos[1], pos[3] ):    
+                obstacle_map[ (int)(x), (int)(y) ] = 0
+
+    return obstacle_map
+
+ori_obstacle_map = get_obstacle()
+g_obstacle_map = get_obstacle()
+
+def update(obstacle):
+    # print( obstacle )
+    global g_obstacle_map
+    g_obstacle_map = ori_obstacle_map
+    for( xx, yy ) in obstacle:
+        for x in np.arange( xx / 0.02 - 7.5, xx / 0.02 + 7.5 ):
+            for y in np.arange( yy / 0.02 - 7.5, yy / 0.02 + 7.5 ):    
+                g_obstacle_map[ (int)(x), (int)(y) ] = 0
+
+
+def show( map, path ):
+    x_0, y_0 = [], []
+    x_1, y_1 = [], []
+    
+    for i in range( 0, 405 ):
+        for j in range( 0, 225 ):
+            if( map[i][j] == 0 ):
+              x_1.append(i)
+              y_1.append(j)  
+
+    for i in path:
+        y_0.append( i[0] )
+        x_0.append( i[1] )
+
+    ppl.clf()
+    ppl.plot( x_1, y_1, '.g' )
+    ppl.plot( x_0, y_0, '.b' )
+    ppl.show( block = True )
+
 def main():
     img = imread(MAP_IMG)
     img1 = imread(MAP_IMG1)
     print( len(img), len(img1) )
     start = [ 100 , 100 ]
-    goal = [ 200, 200 ]
+    goal = [ 200, 250 ]
     fig = ppl.gcf()
     fig.clf()
     ax = fig.add_subplot(1, 1, 1)
@@ -204,7 +273,7 @@ def main():
     ppl.plot( x, y, '.b' )
     ppl.show()
     '''
-
+    show( get_obstacle(), path )
 
 
 main()
