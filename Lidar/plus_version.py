fdaf7abecfb1a44cc761665cbd3a32229b493d94
LarryZhang <u3594786@connect.hku.hk>
2022-07-09 20:14:26 +0800

    Update x,y check function

--- Lidar/plus_version.py
+++ Lidar/plus_version.py
@@ -11,16 +11,56 @@ status_y = 0
 ERROR_X = 0
 ERROR_Y = 0
 
-def check_x( temp_x, temp_y, occupancy_map ):
+def check_x( system_axis, temp_x, temp_y, occupancy_map ):
     global status_x
     global ERROR_X
+    sys_x = system_axis
     obstacle_map = re.get_obstacle()
+    left_x, right_x = -1, -1
+    
+    for i in range( 0, 75 ):
+        if( occupancy_map[i][75] == 8 ):
+            left_x = i
+            break
+    left_dis = 75 - left_x
+    dis_error = 999999
+    if( left_x != -1 ):
+        x_pos, y_pos = temp_x / 0.02, temp_y / 0.02
+        for y in range( y_pos - 16, y_pos + 16 ):
+            if( y >= 0 and y <= 224 ):
+                for i in range(  x_pos, x_pos - left_dis - 30, -1 ):
+                    if( ( i >= 0 and i <= 404 ) and obstacle_map[ i + 150 ][ y + 150 ] == 1 ):
+                        if( abs( x_pos - i - left_dis ) < dis_error ):
+                            dis_error = abs( x_pos - i - left_dis )
+                            real_x_pos = i + left_dis
+        status_x = 1
+        ERROR_X = real_x_pos * 0.02 - sys_x
+
+    for i in range( 76, 150 ):
+        if( occupancy_map[i][75] == 8 ):
+            right_x = i
+            break
+    right_dis = right_x - 75
+    dis_error = 999999
+    if( right_x != -1 ):
+        x_pos, y_pos = (int)( temp_x / 0.02 ), (int)(temp_y / 0.02)
+        for y in range( y_pos - 16, y_pos + 16 ):
+            if( y >= 0 and y <= 224 ):
+                for i in range(  x_pos, x_pos + right_dis + 30 ):
+                    if( ( i >= 0 and i <= 404 ) and obstacle_map[ i + 150 ][ y + 150 ] == 1 ):
+                        if( abs( i - x_pos - right_dis ) < dis_error ):
+                            dis_error = abs( i - x_pos - right_dis )
+                            real_x_pos = i - right_dis
+        status_x = 1
+        ERROR_X = real_x_pos * 0.02 - sys_x
+    
+
 
 def check_y( temp_x, temp_y, occupancy_map ):
     global status_y
     global ERROR_Y
     obstacle_map = re.get_obstacle()
-    
+
 
 def test_function( vector_data, laser_data ):
 
@@ -29,7 +69,7 @@ def test_function( vector_data, laser_data ):
 
     ang, dist = [], []
     for i in range( 0, 61 ):
-        #print( (float)( -vector_data[0][2] + ( 135 * ( i - 30 ) / 30 * pi / 180) )  )
+        # print( (float)( -vector_data[0][2] + ( 135 * ( i - 30 ) / 30 * pi / 180) )  )
         ang.append((float)( pi * 0.5 - vector_data[0][2] + ( 135 * ( i - 30 ) / 30 * pi / 180) ))
         dist.append( (float)(laser_data[i]) )
 
@@ -40,7 +80,7 @@ def test_function( vector_data, laser_data ):
         occupancy_map = re.lidar_to_gird_map( ang, dist )
         temp_x, temp_y = re.lidar_mapping( vector_data, laser_data )
         if( status_x == 0 ):
-            if( check_x( temp_x, temp_y, occupancy_map ) ):
+            if( check_x( vector_data[0][0], temp_x, temp_y, occupancy_map ) ):
                 x = vector_data[0][0] + ERROR_X
             else:
                 x = temp_x
@@ -50,7 +90,7 @@ def test_function( vector_data, laser_data ):
                 y = vector_data[0][1] + ERROR_Y
             else:
                 y = temp_y
-
+    print( status_x, ERROR_X )
     return x, y 
         
 
--- Lidar/test2.py
+++ Lidar/test2.py
@@ -2,7 +2,7 @@ from cmath import pi
 from Cogenvdecoder.CogEnvDecoder import CogEnvDecoder
 import numpy as np
 import cv2
-import lidar_data_mapping_2 as lidar_data_mapping_2
+import plus_version as lidar_data_mapping_2
 
 m_x = []
 m_y = []
@@ -11,15 +11,17 @@ def check_state( state, info=None ):
     laser_data = np.array(state["laser"])
     vector_data = state["vector"]
     # print("=======================state check====================")
-    print("self pose: {}, self info: {}, enemy active: {}, enemy pose: {}, enemy_info: {}".format(vector_data[0], vector_data[1], vector_data[2], vector_data[3], vector_data[4]))
-    x, y = lidar_data_mapping_2.lidar_mapping( vector_data, laser_data )
-    print( "heihei", vector_data[0][0], vector_data[0][1], x, y )
+    # print("self pose: {}, self info: {}, enemy active: {}, enemy pose: {}, enemy_info: {}".format(vector_data[0], vector_data[1], vector_data[2], vector_data[3], vector_data[4]))
+    q, w = vector_data[0][0], vector_data[0][1]
+    x, y = lidar_data_mapping_2.test_function( vector_data, laser_data )
+    # print( "heihei", vector_data[0][0], vector_data[0][1], x, y )
     if( abs(vector_data[0][0] - x) > 0.6 or abs(vector_data[0][1] - y) > 0.6 ):
         pass
     else:
-        m_x.append( vector_data[0][0] - x )
-        m_y.append( vector_data[0][1] - y )
-        print( "mean", np.mean(m_x), np.mean(m_y) )
+        print( "diff:", q - x )
+        # m_x.append( vector_data[0][0] - x )
+        # m_y.append( vector_data[0][1] - y )
+        # print( "mean", np.mean(m_x), np.mean(m_y) )
     # print("-----------------------end check---------------------")
 
 env = CogEnvDecoder(env_name="../../mac_v2/cog_sim2real_env.app", no_graphics=False, time_scale=1, worker_id=1) # mac os
@@ -31,10 +33,10 @@ for i in range(num_episodes):
     obs = env.reset()
     vector_data = obs["vector"]
     dynamic_obstacles = [vector_data[5][:2], vector_data[6][:2], vector_data[7][:2], vector_data[8][:2], vector_data[9][:2]]
-    lidar_data_mapping_2.update( dynamic_obstacles )
+    # lidar_data_mapping_2.update( dynamic_obstacles )
     for j in range(num_steps_per_episode):
         # action = env.action_space.sample()
-        action = [ 0, 0, 0, 0 ]  # [vx, vy, vw, fire]; vx: the velocity at which the vehicle moves forward, vy: the velocity at which the vehicle moves to the left, vw: Angular speed of the vehicle counterclockwise rotation, fire: Shoot or not
+        action = [ 1, 0, 0, 0 ]  # [vx, vy, vw, fire]; vx: the velocity at which the vehicle moves forward, vy: the velocity at which the vehicle moves to the left, vw: Angular speed of the vehicle counterclockwise rotation, fire: Shoot or not
         obs, reward, done, info = env.step(action)
         #cv2.imshow("color_image", obs["color_image"])
         #cv2.waitKey(1)
